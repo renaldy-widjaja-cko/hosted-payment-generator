322e364ff796bf902f58ceab526319422dc83672
Renaldy Widjaja <renaldy.widjaja@checkout.com>
2021-07-22 01:03:29 +0800

    finished hosted payment page

--- src/store/index.js
+++ src/store/index.js
@@ -36,8 +36,9 @@ export default new Vuex.Store({
     },
     POST_PAYMENT(state, data) {
       axios.defaults.headers.common["Authorization"] = state.secret;
+      var formData = {};
       if (state.type == HOSTED_TYPE.PAYMENT_LINK) {
-        var formData = constructPaymentLinkData(data, state)
+        formData = constructPaymentLinkData(data, state)
         axios.post("/payment-links", formData).then(
           result => {
             state.responseLink = result.data._links.redirect.href;
@@ -45,9 +46,55 @@ export default new Vuex.Store({
           state.responseLink = exception;
         })
       }
+      else if (state.type == HOSTED_TYPE.HOSTED_PAYMENT_PAGE){
+        formData = constructHostedPaymentData(data, state);
+        axios.post("/hosted-payments", formData).then(
+          result => {
+            state.responseLink = result.data._links.redirect.href;
+          }).catch(exception => {
+            state.responseLink = exception;
+          })
+      }
     }
   }
 });
+
+function constructHostedPaymentData(data, state) {
+  return {
+    "amount": data.TotalAmount,
+    "currency": data.Currency,
+    "reference": data.Reference,
+    "description": data.Description,
+    "customer": {
+      "name": data.FirstName + " " + data.LastName,
+      "email": data.Email
+    },
+    "billing": {
+      "address": {
+        "address_line1": data.Address1,
+        "address_line2": data.Address2,
+        "city": data.City,
+        "state": data.State,
+        "zip": data.Zip,
+        "country": data.Country
+      },
+      "phone": {
+        "country_code": data.PhoneCountryCode,
+        "number": data.Phone
+      }
+    },
+    "products": data.Products,
+    "3ds": {
+      "enabled": true,
+      "attempt_n3d": false
+    },
+    "success_url": state.successUrl,
+    "failure_url": state.failureUrl,
+    "cancel_url": state.cancelUrl,
+    "locale": "en-GB"
+  };
+}
+
 
 function constructPaymentLinkData(data, state) {
   return {
